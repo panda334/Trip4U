b61b53588d31e95334f97ce674f954be6e6016cf
yousefpanda <yousef.youseff.ref@gmail.com>
2024-11-11 23:09:52 +0300

    final final

--- database/migrations/2024_10_13_140549_create_billing_details_table.php
+++ database/migrations/2024_10_13_140549_create_billing_details_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone_number');
             $table->text('address');
-            $table->unsignedBigInteger('booking_id'); // Foreign key
             $table->foreignIdFor(Booking::class)->constrained();
             $table->timestamps();
         });
